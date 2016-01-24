36eea32b8bb9193f97b3a48e291bbe38f7496076
jeremythuff <huff@library.tamu.edu>
2016-01-24 00:04:00 -0600

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  output: { path: __dirname, filename: 'bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+};
